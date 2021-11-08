e2a26fb32f6fad09c87a4c283b55e0a5dd95f422
aromegar <a_romegar@LAPTOP-2137LP05.localdomain>
2021-11-08 22:46:38 +0100

    otra

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Pipeline
 IronHack week3 proyect - Creating a pipeline
+
+## Objetivo
+El objetivo de este proyecto es construir un ejecutable que cargue datos, los manipule y nos devuelva información de manera automática
--- src/visual_functions.py
+++ src/visual_functions.py
@@ -18,3 +18,30 @@ def ordenadito(city,d0,d1,d2):
 
     return pd.DataFrame(diccio)
     
+
+from PIL import Image
+import requests
+from io import BytesIO
+
+def dibujar(tit):
+    """
+    dibuja una imagen a partir de un titulo
+    """
+    url = f"http:{tit}"
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    return img
+
+import matplotlib.pyplot as plt
+
+def dibu3(data):
+    """
+    dibuja los 3 iconos en linea correspondientes a los 3 dias de un dataframe
+    """
+    fig = plt.figure(figsize=(5, 5))
+    
+    for i in range(1,4):
+        image=dibujar(data.T[4][i])
+        fig.add_subplot(1, 3, i)
+        plt.imshow(image)
+        plt.axis('off')
